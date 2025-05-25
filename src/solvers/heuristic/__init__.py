dc6a3017a0d4f50e9796a5379783616dd7e303b9
Miguel Alessandro Mini H. <45865868+peon-pasado@users.noreply.github.com>
2025-05-25 15:17:50 -0300

    update solutions

---
+++ src/solvers/heuristic/__init__.py
@@ -0,0 +1,6 @@
+"""
+This module contains heuristic solvers for the Wave Order Picking problem.
+Includes implementations of:
+- Simulated Annealing
+- Greedy with randomization
+""" 
---
+++ src/solvers/hybrid/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module contains hybrid solvers for the Wave Order Picking problem.
+Includes implementations that combine:
+- Heuristics with exact methods
+- Ratio optimization with different techniques
+- Iterative improvements
+""" 
